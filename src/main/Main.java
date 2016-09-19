a39cfb40e7f5b229e72dcf4e3c3940e18d511455
ElTangoBandito <Volfurious@gmail.com>
2016-09-19 16:48:42 -0700

    testing

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,20 +4,42 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Main {
-	//private static int userPort;
+	
 	public static void main(String[] args){
-		
-		/*
-		Scanner baseScanner = new Scanner(System.in);
-		String userInput = baseScanner.next();
-		userPort = Integer.parseInt(userInput);	
-		*/
-		
 		try {
-			ServerSocket socket = new ServerSocket(4470);
+			ServerSocket socket = new ServerSocket(getPort());
+			System.out.println(socket.getLocalPort());
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 
+	public static int getPort(){
+		int userPort = 0;
+		while(true){
+			Scanner baseScanner = new Scanner(System.in);
+			String userInput = baseScanner.nextLine();
+			String[] userArgs = userInput.split("\\s+");
+			
+			if (userArgs.length == 2){
+				if (userArgs[0].equals("/chat")){
+					try{
+						userPort = Integer.parseInt(userArgs[1]);
+						if (userPort >= 1000 && userPort <= 65536){
+							break;
+						}
+					}catch(Exception e){
+						e.printStackTrace();
+						userPort = 0;
+					}
+				}
+			}
+			else{
+				continue;
+			}
+		}
+		return userPort;
+		
+	}
+	
 }
