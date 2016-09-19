81c8a3fc189ac3ae5be50d7892d49aa4e553c917
ElTangoBandito <Volfurious@gmail.com>
2016-09-19 15:51:30 -0700

    Second commt

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,23 @@
 package main;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Main {
+	//private static int userPort;
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		
+		/*
+		Scanner baseScanner = new Scanner(System.in);
+		String userInput = baseScanner.next();
+		userPort = Integer.parseInt(userInput);	
+		*/
+		
+		try {
+			ServerSocket socket = new ServerSocket(4470);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
+
 }
