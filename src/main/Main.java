7792a22a2e18bef206f9568ebc17a80fd24dd2f0
ElTangoBandito <Volfurious@gmail.com>
2016-10-01 08:43:29 -0700

    Error fixing

--- src/main/Main.java
+++ src/main/Main.java
@@ -27,10 +27,11 @@ public class Main {
                     out.println("Connected on " + time.toString());
                     System.out.print("Connected to client");
                     peerList.add(new Peer(socket, PORTNUMBER));
-                    
+                    /*
                     while (true) {
                     	
                     }
+                    */
                     
                     listPeers();
                 } finally {
